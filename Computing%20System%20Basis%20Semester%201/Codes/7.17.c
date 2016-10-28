b7390547d4aebc24d8c2ecb0dd770c07ac5aa8ed
Chen Junda <viccrubs@users.noreply.github.com>
2016-10-28 15:06:19 +0800

    Added 7.17

---
+++ Computing%20System%20Basis%20Semester%201/Codes/7.17.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+int main(){
+    int status=0;
+    int counter=0;
+    char nextChar;
+	printf("Input a string:");
+    do{
+    	scanf("%c",&nextChar);
+		switch(status){
+	    	case 0:
+	        	if (nextChar=='i'){
+				    status=1;
+				}
+				break;
+	    	case 1:
+				switch(nextChar){
+					case 'n':
+						status=2;
+						break;
+					case 'i':
+						status=1;
+						break;
+					default:
+						status=0;
+						break;
+				}
+				break;
+			case 2:
+				switch(nextChar){
+					case 't':
+						counter++;
+						status=0;
+						break;
+					case 'i':
+						status=1;
+						break;
+					default:
+						status=0;
+						break;
+				}
+		}
+    } while (nextChar != '\n');
+	
+	printf("Number of \"int\": %d\n",counter);
+    system("pause");
+    return 0;
+}
