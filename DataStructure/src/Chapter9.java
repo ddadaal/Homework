ffe48f7760388855aa23958495b33c9ec4d16cc6
Chen Junda <smallda@outlook.com>
2017-12-15 00:23:36 +0800

    check for duplicate cycle

--- DataStructure/src/Chapter9.java
+++ DataStructure/src/Chapter9.java
@@ -1,4 +1,5 @@
 import util.Graph;
+import util.Graph.CycleList;
 public class Chapter9 {
 	public static void main(String[] arg) {
 		Graph<String> graph = new Graph<>();
@@ -10,10 +11,13 @@ public class Chapter9 {
 		graph.addEdge(0, 1);
 		graph.addEdge(1, 2);
 		graph.addEdge(2, 3);
-		graph.addEdge(1, 3);
+		graph.addEdge(3, 1);
 		graph.addEdge(3, 0);
-		if (!graph.outputCycles()) {
+		Graph<String>.CycleList cl = graph.outputCycles();
+		if (cl.size()==0){
 			System.out.println("no graph detected.");
+		} else {
+			System.out.println(cl.toString());
 		}
 	}
 }
--- DataStructure/src/util/Graph.java
+++ DataStructure/src/util/Graph.java
@@ -1,11 +1,15 @@
 package util;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.LinkedList;
+
+
 
 public class Graph<T> {
 	ArrayList<T> nodes = new ArrayList<>();
 	
 	ArrayList<LinkedList<Integer>> edges = new ArrayList<>();
+	Comparator<Integer> comparator = (x,y) -> x-y;
 	
 	public int addNode(T node) {
 		nodes.add(node);
@@ -23,41 +27,92 @@ public class Graph<T> {
 		return -1;
 	}
 	
+	public class CycleList {
+		private ArrayList<Cycle> cycleList = new ArrayList<>();
+		
+		public void addCycle(Cycle cycle) {
+			for (Cycle c: cycleList) {
+				if (c.isEquivalent(cycle)) {
+					return;
+				}
+			}
+			cycleList.add(cycle);
+		}
+		
+		public int size() {
+			return cycleList.size();
+		}
+		
+		@Override
+		public String toString() {
+			StringBuilder builder = new StringBuilder();
+			for (Cycle c: cycleList) {
+				builder.append(c.toString());
+				builder.append("\n");
+			}
+			return builder.toString();
+		}
+	}
+	
+	public class Cycle {
+
+		public ArrayList<Integer> points;
+		public Cycle(ArrayList<Integer> points) {
+			this.points = new ArrayList<>(points);
+			this.points.sort(comparator);
+		}
+		
+		@Override
+		public String toString() {
+			StringBuilder builder = new StringBuilder();
+			for (int i=0;i<points.size();i++) {
+				builder.append(nodes.get(points.get(i)));
+				builder.append("->");
+				if (i == points.size()-1) {
+					builder.append(nodes.get(points.get(0)));
+				}
+			}
+			return builder.toString();
+		}
+		
+		public boolean isEquivalent(Cycle c) {
+			if (c.points.size()!= this.points.size()) {
+				return false;
+			}
+			for (int i=0;i<c.points.size();i++) {
+				if (!points.get(i).equals(c.points.get(i))) {
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+	
 	public void addEdge(int start, int end) {
 		LinkedList<Integer> node = edges.get(start);
 		node.add(end);
 	}
 	
-	public boolean outputCycles() {
+	public CycleList outputCycles() {
 		int size =nodes.size();
-		boolean checked = false;
+		CycleList cl = new CycleList();
 		for (int i=0;i<size;i++) {
-			checked= checked | checkCycle(i,i,new ArrayList<>());
+			checkCycle(i,i,new ArrayList<>(), cl);
 		}
-		return checked;
+		return cl;
 	}
 	
-	private boolean checkCycle(int si, int ci, ArrayList<Integer> stack) {
+	private void checkCycle(int si, int ci, ArrayList<Integer> stack, CycleList cl) {
 		if (si == ci && stack.size() != 0) {
-			for (int i=0;i<stack.size();i++) {
-				System.out.print(nodes.get(stack.get(i)));
-				if (i != stack.size()-1) {
-					System.out.print("->");
-				} else {
-					System.out.println("->" + (nodes.get(stack.get(0))));
-				}
-			}
-			return true;
+			cl.addCycle(new Cycle(stack));
 		} else if (stack.size() == nodes.size()) {
-			return false;
+			return;
 		} else {
-			boolean result = false;
 			stack.add(ci);
 			for (int i : edges.get(ci)) {
-				result = result | checkCycle(si, i, stack);
+				checkCycle(si, i, stack, cl);
 			}
 			stack.remove(stack.size()-1);
-			return result;
 		}
 
 	}
