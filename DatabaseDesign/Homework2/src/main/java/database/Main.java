54f31439c06bf099fac2afc13cd679a1a8813f52
Chen Junda <smallda@outlook.com>
2018-10-21 17:18:45 +0800

    add project for homework 2

---
+++ DatabaseDesign/Homework2/src/main/java/database/Main.java
@@ -0,0 +1,19 @@
+package database;
+
+import database.mapper.MainMapper;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.InputStream;
+
+public class Main {
+    public static void main(String[] args) {
+        String resource = "/mybatis-config.xml";
+        InputStream inputStream = Main.class.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        MainMapper mapper = sqlSession.getMapper(MainMapper.class);
+        System.out.println(mapper.selectTest());
+    }
+}
---
+++ DatabaseDesign/Homework2/src/main/java/database/mapper/MainMapper.java
@@ -0,0 +1,5 @@
+package database.mapper;
+
+public interface MainMapper {
+    int selectTest();
+}
