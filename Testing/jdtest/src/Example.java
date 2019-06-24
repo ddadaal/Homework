c14395b63872e7888e4934ce9ae879b4c7b21c22
Chen Junda <smallda@outlook.com>
2019-06-24 10:50:51 +0800

    add jdtest and meituan perf

--- Testing/jdtest/src/Example.java
+++ Testing/jdtest/src/Example.java
@@ -1,9 +1,15 @@
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -21,80 +27,82 @@ public class Example {
 	
     public static void test(WebDriver driver) {
     	
+    	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    	
     	Example.driver = driver;
     
     	
 //        // TODO Test script
         driver.get("https://jd.com/");
 
-        xpath("/html/body[@class='index']/div[@id='app']/div[@id='shortcut']/div[@class='w']/ul[@class='fl']/li[@id='ttbar-mycity']/div[@class='dt cw-icon ui-areamini-text-wrap']/span[@class='ui-areamini-text']").click();
-        xpath("/html/body[@class='index']/div[@id='app']/div[@id='shortcut']/div[@class='w']/ul[@class='fl']/li[@id='ttbar-mycity']/div[@class='dd dropdown-layer']/div[@class='ui-areamini-content-wrap']/div[@class='ui-areamini-content']/div[@class='ui-areamini-content-list']/div[@class='item'][12]/a[@class='selected']").click();
+        selector("#ttbar-mycity > div.dt.cw-icon.ui-areamini-text-wrap").click();
+        selector("#ttbar-mycity > div.dd.dropdown-layer > div.ui-areamini-content-wrap > div > div > div:nth-child(12) > a").click();
         
-        selector("a[href='//book.jd.com/']").click();
+        selector("#J_cate > ul > li:nth-child(14) > a:nth-child(1)").click();
+        toNewPage();
+
+    	driver.get("https://book.jd.com");
+    	
+        selector("#qiye-activity > a").click();
+        selector("#key").sendKeys("软件测试朱少民");
+        selector("#search-2014 > div > button").click();
+        
+        selector("#J_selector > div:nth-child(2) > div > div.sl-value > div.sl-v-list > ul > li:nth-child(1) > a").click();
+
+        
+        selector("#J_selectorPrice > div.f-price-set > div:nth-child(1) > input").sendKeys("35");
+        
+        selector("#J_selectorPrice > div.f-price-set > div:nth-child(3) > input").sendKeys("60");
+        
+        selector("#J_selectorPrice > div.f-price-set > div:nth-child(3) > input").click();
+        
+        selector("#J_selectorPrice > div.f-price-edit > a.item2.J-price-confirm").click();
+        
+        selector("#J_filter > div.f-line.top > div.f-sort > a:nth-child(5)").click();
+        
         sleep(1000);
-//        
-        // switch to new window
+        
+        selector("#J_filter > div.f-line.top > div.f-sort > a:nth-child(5)").click();
+        
+        selector("#J_store_selector > div.ui-area-text-wrap").click();
+    
+        selector("#J_store_selector > div.ui-area-content-wrap.ui-area-w-max > div.ui-area-tab > a:nth-child(3)").click();
+        
+        selector("#J_store_selector > div.ui-area-content-wrap.ui-area-w-max > div.ui-area-content > div.ui-switchable-panel.ui-switchable-panel-selected > ul > li:nth-child(8) > a").click();
+        
+        sleep(1000);
+        selector("#J_feature > ul > li:nth-child(1) > a").click();
+        
+        sleep(1000);
+        
+        List<WebElement> goods = driver.findElements(By.cssSelector("#J_goodsList > ul > li"));
+
+        
+        goods.get(2).findElement(By.cssSelector("div > div.p-name > a")).click();
         
         toNewPage();
-    
         
-        WebElement input = xpath("/html/body/div[@class='w']/div[@id='search-2014']/div[@class='form']/input[@id='key']");
-        input.sendKeys("软件测试朱少民");
-        input.sendKeys(Keys.ENTER);
-        sleep(3000);
+        selector("#choose-btns > div.choose-amount.fl > div > a.btn-add").click();
         
-        xpath("/html/body/div[@id='J_searchWrap']/div[@id='J_container']/div[@id='J_selector']/div[@class='J_selectorLine s-category'][2]/div[@class='sl-wrap']/div[@class='sl-value']/div[@class='sl-v-list']/ul[@class='J_valueList']/li[1]/a").click();
-       
-
+        selector("#InitCartUrl").click();
         
-        driver.findElement(By.xpath("/html/body/div[@id='J_searchWrap']/div[@id='J_container']/div[@id='J_main']/div[@class='m-list']/div[@class='ml-wrap']/div[@id='J_filter']/div[@class='f-line top']/div[@id='J_selectorPrice']/div[@class='f-price-set']/div[@class='fl'][1]/input[@class='input-txt']")).sendKeys("35");
-        driver.findElement(By.xpath("/html/body/div[@id='J_searchWrap']/div[@id='J_container']/div[@id='J_main']/div[@class='m-list']/div[@class='ml-wrap']/div[@id='J_filter']/div[@class='f-line top']/div[@id='J_selectorPrice']/div[@class='f-price-set']/div[@class='fl'][2]/input[@class='input-txt']")).sendKeys("60");
-//        driver.findElement(By.cssSelector(".J-price-confirm")).click();
+        selector("#GotoShoppingCart").click();
         
-        // hover to input and click the button
+        selector(".decrement").click();
         
-        xpath("/html/body/div[@id='J_searchWrap']/div[@id='J_container']/div[@id='J_main']/div[@class='m-list']/div[@class='ml-wrap']/div[@id='J_filter']/div[@class='f-line top']/div[@id='J_selectorPrice']/div[@class='f-price-set']/div[@class='fl'][2]/input[@class='input-txt']").click();
-        selector(".J-price-confirm").click();
-        
-        driver.findElements(By.cssSelector(".f-sort > a")).get(4).click();
         sleep(500);
-        driver.findElements(By.cssSelector(".f-sort > a")).get(4).click();
-
         
-       selector("#J_store_selector").click();
-        driver.findElements(By.cssSelector(".ui-switchable-item")).get(2).click();
-       selector("a[data-id='3377']").click();
-        
-        sleep(2000);
-        selector("a[data-field='wtype']").click();
-        
-        driver.findElements(By.cssSelector(".gl-i-wrap")).get(2).click();
-        sleep(1000);
-//      
-      // switch to new window
-      
-  	toNewPage();
-      
-    	
-    	driver.get("https://item.jd.com/11563286.html?dist=jd");
-    	
-      selector(".btn-add").click();
-      selector(".btn-append").click();
-      
-      toNewPage();
-      
-      selector(".btn-addtocart").click();
-
-      sleep(3000);
-      
-      selector(".decrement").click();
-      
+        selector(".submit-btn").click();
+    }
+    
+    
+    static void waitForPresence(WebElement element) {
+    	new WebDriverWait(driver, 6000).until(ExpectedConditions.elementToBeClickable(element));
     }
     
     static void toNewPage() {
-      for (String winHandle: driver.getWindowHandles()) {
-  	driver.switchTo().window(winHandle);
-  }
+    	ArrayList<String> list = new ArrayList<>(driver.getWindowHandles());
+    	driver.switchTo().window(list.get(list.size()-1));
     }
     
     static WebElement xpath(String xpath) {
